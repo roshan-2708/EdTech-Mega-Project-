439f52fef48e9652adec8ee8ebde73a20db8b5db
Roshan4546 <roshanpatra275@gmail.com>
2026-01-19 18:41:33 +0530

    fix api base url

--- frontend/tailwind/src/services/apiConnecter.js
+++ frontend/tailwind/src/services/apiConnecter.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
-    timeout: 60000,
+    timeout: 30000,
     withCredentials: true,
 });
 
