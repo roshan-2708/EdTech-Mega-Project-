7159ecc4e3fdf5f2031581cdf0656946eacc05f5
Roshan4546 <roshanpatra275@gmail.com>
2026-01-19 13:04:55 +0530

    fix bug

--- frontend/tailwind/src/services/apiConnecter.js
+++ frontend/tailwind/src/services/apiConnecter.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
-    timeout: 30000,
-    withCredentials: true,
+    timeout: 60000,
+    withCredentials: false,
 });
 
 // ✅ REQUEST INTERCEPTOR (AUTO ADD TOKEN)
