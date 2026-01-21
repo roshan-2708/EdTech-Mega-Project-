55e84b48b8b29dc394a67ed6447f2b0b260d1daf
Roshan4546 <roshanpatra275@gmail.com>
2026-01-21 21:00:31 +0530

    change deafult domain

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -43,7 +43,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const mailSender = async (email, title, body) => {
     try {
         await resend.emails.send({
-            from: "StudyNotion <onboarding@resend.dev>", // default domain
+            from: "StudyNotion <onboarding@resend.dev>",
             to: email,
             subject: title,
             html: body,
