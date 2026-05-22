f051843a4b4535bb3852ae7b6710eee26cf60c35
Roshan4546 <roshanpatra275@gmail.com>
2026-05-22 21:02:06 +0530

    change passport.js file

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../model/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://edtech-mega-project.onrender.com/api/v1/auth/google/callback",
+    callbackURL: "https://edtech-mega-project.onrender.com/api/v1/auth/google/callback",
     proxy: true
   },
   async (accessToken, refreshToken, profile, done) => {
