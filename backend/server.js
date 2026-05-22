ad9eb4c7258ee85d7462f571b6ad66a3d0461656
Roshan4546 <roshanpatra275@gmail.com>
2026-05-22 21:19:21 +0530

    server page refactor

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,7 @@ app.use(passport.initialize());
 
 // API Routes
 app.use("/api/v1/auth", userRoutes);
+app.use("/api/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/course", courseRoutes);
